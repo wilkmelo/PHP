b63bcd1e3706c781239d6bedf26ddad3a5711264
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-23 17:56:06 -0300

    Criei o ex02.

---
+++ aula02/ex02.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis em PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <?php
+        $nome = "Gustavo";
+        $idade = 36;
+        echo "$nome tem $idade anos!";
+         ?>
+    </div>
+
+</body>
+</html>
