40b5c83be1a6ef731e70f0aeca2bbf65c14b0e55
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-23 17:27:32 -0300

    Criei o ex01.

---
+++ ex01.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Primeiro exemplo de php</title>
+    <style>
+        h2 {
+            color:#80a2ff;
+            text-shadow:1px 1px 1px black;
+        }
+    </style>
+</head>
+<body>
+    <h1>Testando PHP</h1>
+    <?php
+    echo "<h2>Olá, Mundo!</h2>";
+    ?>
+</body>
+</html>
