e0aaaa9d10ec3bd233cda60a024e95918055ec48
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-24 17:46:09 -0300

    ex10.

---
+++ aula04/ex10.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores de Atribuição</title>
+</head>
+<body>
+    <div>
+        <?php
+        $x = "abc";
+        $$x = "def";
+        echo "O conteúdo da variável X e $x";
+        echo "A variável criada recebeu o valor $abc";
+        ?>
+    </div>
+</body>
+</html>
