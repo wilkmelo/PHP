cfacb0a51141f8ed3b1c1907724573abdbffb619
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-24 09:20:36 -0300

    aula03 e ex03.

---
+++ aula03/ex03.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Aritméticos</title>
+</head>
+<body>
+    <div>
+        <?php
+        $n1 = 3;
+        $n2 = 2;
+        $s = $n1 + $n2;
+        echo "A soma entre $n1 e $n2 é igual a $s";
+        ?>
+    </div>
+</body>
+</html>
