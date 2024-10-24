87ca8667a96cfbc84da60df8a73d917eabdcc175
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-24 10:19:47 -0300

    Atualizei.

--- aula03/ex04.php
+++ aula03/ex04.php
@@ -10,11 +10,13 @@
         <?php
         $n1 = 3;
         $n2 = 2;
+        $m = ($n1 + $n2)/2;
         echo "A soma vale ". ($n1+$n2);
         echo "<br/>A subtração vale ". ($n1-$n2);
         echo "<br/>A multiplicação vale ". ($n1*$n2);
         echo "<br/>A divisão vale ". ($n1/$n2);
         echo "<br/>O modulo vale ". ($n1%$n2);
+        echo "<br/>A media vale $m";
         ?>
     </div>
 </body>
