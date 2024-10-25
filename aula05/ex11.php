7fe6d94faea338d3d34a6727d63424918580c4a7
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-25 08:49:25 -0300

    ex11.

---
+++ aula05/ex11.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Relacionais</title>
+</head>
+<body>
+    <div>
+        <?php
+        $n1 = $_GET["a"];
+        $n2 = $_GET["b"];
+        $tipo = $_GET["op"];
+        echo "Os valores passados foram $n1 e $n2 <br/>";
+        $r = ($tipo == "s") ? $n1+$n2  :  $n1*$n2;
+        echo "O resulto será $r";
+        ?>
+    </div>
+</body>
+</html>
