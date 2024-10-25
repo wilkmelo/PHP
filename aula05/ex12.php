15af12c74fc5803796b5673a2b6cc9f0053226f3
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-25 08:55:16 -0300

    ex12.

---
+++ aula05/ex12.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operadores Relacionais</title>
+</head>
+<body>
+    <div>
+        <?php
+        $a = 3;
+        $b = "3";
+        $r = ($a === $b) ?"SIM":"NÃO";
+        echo "As variáveis A e B são iguais? $r";
+        ?>
+    </div>
+</body>
+</html>
