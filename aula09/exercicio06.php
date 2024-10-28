e1ff1d39618e68f9c8a3706cf66213a76125706d
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-28 14:43:09 -0300

    Atualizei o exercicio06.

--- aula09/exercicio06.php
+++ aula09/exercicio06.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="Pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,9 +7,17 @@
 </head>
 <body>
     <div>
-        <?php
-       
-        ?>
+        <form action="exercicio06.php" method="get">
+            <?php
+            $c = 1;
+            while($c <= 5){
+           echo  "Valor $c: <input type='number' name='v$c' max='100' min='0' value='0'/><br/>";
+           $c ++;
+        }
+            ?>
+            <input type="submit" value="Enviar" class="botao">
+
+        </form>
     </div>
 </body>
 </html>
