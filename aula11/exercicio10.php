0075ef0c17e3ecfeee4cc3652a0152083246e53b
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-29 14:53:24 -0300

    Criei o exercício10.

---
+++ aula11/exercicio10.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Estrutura de Repetição For</title>
+</head>
+<body>
+    <div>
+        <?php
+        for ($c = 1; $c <= 10; $c ++) {
+            echo $c;
+        }
+        ?>
+    </div>
+</body>
+</html>
