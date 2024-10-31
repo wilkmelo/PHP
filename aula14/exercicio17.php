2608f05a3d918d0047540d50282af7746ce180b3
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-31 14:06:55 -0300

    Exercício17.

---
+++ aula14/exercicio17.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DocuFunções String em PHP</title>
+</head>
+<body>
+    <div>
+        <?php
+        $p = "Leite";
+        $sp = 4.5;
+        Echo "O $p custa R$ $pr";
+        ?>
+    </div>
+</body>
+</html>
