3fc28ee7c7552789af93a70a61e3be5a2602b8ad
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-10-31 08:54:56 -0300

    Exercício16.

--- aula13/exercicio16.php
+++ aula13/exercicio16.php
@@ -8,14 +8,7 @@
 <body>
     <div>
         <?php
-        function teste (&$x){
-            $x += 2;
-            echo "<p>O valor de X é $x</p>";
-    }
-
-        $a = 3;
-        teste ($a);
-        echo "<p>O valor de A é $a</p>";
+            include "funcoes.php"
         ?>
     </div>
 </body>
