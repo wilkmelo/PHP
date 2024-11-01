3b937b88cfc0a189d9ac46560504cb19e5b6a690
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-01 14:46:06 -0300

    Excercício18.

--- aula14/exercicio18.php
+++ aula14/exercicio18.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funções String em PHP</title>
+    <title>Funções String em PHP Print_r</title>
 </head>
 <body>
     <div>
         <?php
-        $p = "Leite";
-        $pr = 4.5;
-        // Echo "O $p custa R$ " . number_format($pr,2);
-        printf ("O %s custa R$ %.2f", $p, $pr);
+        $v [0] = 4;
+        $v [1] = 8;
+        $v [2] = 3;
+        print_r($v);
         ?>
     </div>
 </body>
