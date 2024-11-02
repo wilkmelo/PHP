2e2c0728242de5028d34c5939741d82cdc9511b7
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-02 15:18:26 -0300

    Exercício24.

---
+++ aula14/exercicio24.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções String em PHP ord/title>
+</head>
+<body>
+    <div>
+        <?php
+            $letra = "C";
+            $cod = ord($letra);
+            print("A letra $letra tem código $cod");
+      ?>
+    </div>
+</body>
+</html>
