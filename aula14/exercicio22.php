535577e1fec80a5195bd4b7391d4c62facc57d3f
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-02 15:08:54 -0300

    Excercício22.

--- aula14/exercicio22.php
+++ aula14/exercicio22.php
@@ -3,14 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funções String em PHP Explode</title>
+    <title>Funções String em PHP implode</title>
 </head>
 <body>
     <div>
         <?php
-       $site = "Curso em Vídeo";
-       $vetor = explode ("", $site);
-       print_r($vetor);
+       $vetro [0] = "Curso";
+       $vetor [1] = "em ";
+       $vetor [2] = "Vídeo";
+       $texto = implode ("#", $vetor);
+       print($texto);
         ?>
     </div>
 </body>
