d377a370f8a8a469e28dd087413c8c6f0cc93af7
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-04 16:35:58 -0300

    Exercício26.

---
+++ aula15/exercicio26.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções String em PHP ucfirst</title>
+</head>
+<body>
+    <div>
+        <?php
+        $nome = "Wilk Melo";
+        $nome2 = ucfirst($nome);
+        echo "Seu nome e $nome2"; 
+        ?>
+        
+    </div>
+</body>
+</html>
