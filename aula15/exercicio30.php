48a2d50414c1f3fb8a7337cf6efa2698ce3f66f9
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-05 14:13:08 -0300

    Exercício30.

--- aula15/exercicio30.php
+++ aula15/exercicio30.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Funções String em PHP Substr</title>
+    <title>Funções String em PHP Str_replace</title>
 </head>
 <body>
     <div>
         <?php
-        $site = "Curso em Vídeo";
-        $sub = substr($site, 0, 5);
-        echo "$sub";
+        $frase = "CGosto de estudar Matemática";
+        $novafrase = str_replace("Matemática", "PHP"; $frase);
+        echo "$novafrase";
         ?>
         
     </div>
