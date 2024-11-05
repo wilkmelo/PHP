00db5505d16d3739aa10a1f6c99617a337ef5f1e
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-05 15:01:47 -0300

    Excercício32.

---
+++ aula16/excercicio32.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vetores e Matrizes</title>
+</head>
+<body>
+    <div>
+        <?php
+        $n = array(3, 5, 8, 2);
+        print_r($n);
+        ?>
+    </div>
+</body>
+</html>
