e1e9e8659296ea17c53ef8ae3984bed0d3a5bdaf
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-08 17:04:28 -0300

    Excercício35.

---
+++ aula16/excercicio35.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vetores e Matrizes</title>
+</head>
+<body>
+    <div>
+        <pre>
+        <?php
+        $c = range(5, 20, 2);
+        foreach ($c as $v) {
+        echo "$v ";
+        }
+        ?>
+        </pre>
+    </div>
+</body>
+</html>
