3170f72398c27a04ec5f5dec0445be8616e29e27
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-09 09:23:01 -0300

    Atualizei o exercício37.

--- aula16/excercicio37.php
+++ aula16/excercicio37.php
@@ -12,6 +12,7 @@
         $m = array ( array (6, 4), 
                     array (4, 9),
                     array (3, 2));
+        $m [0] [1] = $m [2] [0];
         print_r ($m);
         ?>
         </pre>
