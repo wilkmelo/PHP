e08a2c77f6b507a4a76ac684deaf849a2b21237c
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-12 13:28:40 -0300

    Atualizei o exercício38.

--- aula16/excercicio38.php
+++ aula16/excercicio38.php
@@ -9,11 +9,8 @@
     <div>
         <pre>
         <?php
-        $m = array ( array (6, 4), 
-                    array (4, 9),
-                    array (3, 2));
-        $m [0] [1] = $m [2] [0];
-        print_r ($m);
+        $v = array ("A", "J", "M", "X", "K");
+        print_r ($v);
         ?>
         </pre>
     </div>
