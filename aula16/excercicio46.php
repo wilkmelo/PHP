e4a2d68ce5ae7a1bee415b99637978f8fa9b187a
JOSE WILK FERREIRA DE MELO <wilk.jose.sb@gmail.com>
2024-11-13 08:58:46 -0300

    Exercício46.

---
+++ aula16/excercicio46.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vetores e Matrizes</title>
+</head>
+<body>
+    <div>
+        <pre>
+        <?php
+        $v = array ("A", "J", "M", "X", "K");
+        print_r ($v);
+        rsort ($v);
+        print_r ($v);
+        ?>
+        </pre>
+    </div>
+</body>
+</html>
